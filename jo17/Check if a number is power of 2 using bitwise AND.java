b48135a7b04ddebc43e41c229fb92ea21fbded3c
A.Nisanth Noel <nisanthnoel05@gmail.com>
2025-12-12 16:38:21 +0530

    Add files via upload

---
+++ jo17/Check if a number is power of 2 using bitwise AND.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        boolean n = a > 0 && (a & (a - 1)) == 0;;
+        System.out.println(n);
+    }
+}
